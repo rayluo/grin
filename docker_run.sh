d298c65c4c8f1098bfc9faeeb6958518d9ab3579
Ray Luo <rayluo.mba@gmail.com>
2021-01-24 00:04:12 -0800

    New image based on python:3-alpine, font and PySimpleGUI

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,12 @@
+# Usage: docker_run [Dockerfile.alpine | Dockerfile.slim]
+DOCKERFILE=${1-Dockerfile.alpine}
+echo "Building with $DOCKERFILE"
+
 # See also https://stackoverflow.com/questions/45141402/build-and-run-dockerfile-with-one-command#comment116261404_51314059
 IMAGE_NAME=tk_cn_sg
 
 # Build the image with context because we'd need the ./requirements.txt
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME -f $DOCKERFILE .
 
 # Permits the root user on the local machine to connect to X windows display.
 # (It feels more secure than "xhost +" which disables all access control.)
