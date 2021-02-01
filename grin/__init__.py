db50be03f5269bbb2c0e495745a77be042e2ac2d
Ray Luo <rayluo.mba@gmail.com>
2021-01-31 20:05:40 -0800

    Create a hierarchy project structure

---
+++ grin/__init__.py
@@ -0,0 +1,6 @@
+# Preparing to change the previous flat project structure into a hierarchy one.
+# so that future Brython introduction will be able to run
+#       brython-cli --modules
+# See also https://github.com/brython-dev/brython/issues/1603#issuecomment-770375836
+
+from .grin import __version__, GreenInput
